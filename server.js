5739d015ee4bd1b0ae8a2f7d54169db20aaab8a8
vikil_netcore <vikil.lakkavatri@netcorecloud.com>
2024-09-07 17:33:33 +0530

    html loader was added

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname)));
+
+// Route for serving the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './src/public/design-patterns/index.html'));
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
